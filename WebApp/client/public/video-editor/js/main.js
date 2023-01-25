dc5c5af4a897a15c28f447521a38df8d34dde086
Kevin Fischer <106684807+Kevin-Fischer-WSW@users.noreply.github.com>
2023-01-25 11:52:56 -0800

    WIP Populate dropdown with clips to add to editor.

--- WebApp/client/public/video-editor/js/main.js
+++ WebApp/client/public/video-editor/js/main.js
@@ -3,6 +3,8 @@
 let timeline = new Timeline(30, 0.1, 2);
 document.getElementById('timeline-container').appendChild(timeline.element);
 
+let clipDropdown = document.getElementById('clip-dropdown');
+
 // Get a list of the recorded video files
 let clips = [];
 async function GetClips(){
@@ -13,20 +15,31 @@ async function GetClips(){
     let data2 = await resp2.json()
     if (resp2.ok) {
       clips = data2;
-      console.log(clips);
+      for (let i = 0; i < clips.length; i++) {
+        clipDropdown.innerHTML += `<li value="${i}"><a class="dropdown-item">${clips[i].file}</a></li>`;
+        clipDropdown.firstChild.addEventListener('click', () => {
+          if (addAndNotInsertButtonPressedLast){
+            timeline.createClip(clips[i].file, clips[i].duration);
+          }else{
+            timeline.insertClip(timeline.selectedClipIndex, clips[i].file, clips[i].duration);
+          }
+        });
+      }
     }
   }
 }
 
 GetClips();
 
+let addAndNotInsertButtonPressedLast = false;
+
 let addClipButton = document.getElementById('add-clip');
 addClipButton.addEventListener('click', () => {
-  timeline.createClip("clip", 30);
+  addAndNotInsertButtonPressedLast = true;
 });
 let insertClipButton = document.getElementById('insert-clip');
 insertClipButton.addEventListener('click', () => {
-  timeline.insertClip("clip", 30, timeline.selectedClipIndex);
+  addAndNotInsertButtonPressedLast = false;
 });
 let deleteClipButton = document.getElementById('delete-clip');
 deleteClipButton.addEventListener('click', () => {
