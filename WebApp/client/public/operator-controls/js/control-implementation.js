0b908487ec6f9cfc48b6f306c26b432633c9e8f2
Aziz <maziz@twst.com>
2023-03-27 12:17:03 -0400

    UI-fixes. Batch upload only makes a fetch request when all promises are fulfilled. Slides logic - WIP.

--- WebApp/client/public/operator-controls/js/control-implementation.js
+++ WebApp/client/public/operator-controls/js/control-implementation.js
@@ -348,7 +348,8 @@ function appStatusReceived(json) {
   `Stream: ${jsonParsed.streaming ? "Yes": "No"} |
   Recording: ${jsonParsed.recording ? "Yes": "No"} |
   Holding Slide: ${jsonParsed.holdingSlide} |
-  Holding Music: ${jsonParsed.playingHoldingMusic ? "Playing": "Not Playing"}.`
+  Holding Music: ${jsonParsed.playingHoldingMusic ? "Playing" : "Not Playing"} | 
+  Holding Video: ${jsonParsed. playingVideo ? "Playing" : "Not Playing"}.`
 
   if (jsonParsed.inMeeting || jsonParsed.meetingSimulated) {
     validateTracksInPlaylist(jsonParsed.playlist, jsonParsed.currentlyPlayingIndex)
@@ -423,6 +424,7 @@ function appStatusReceived(json) {
     }
 
   } else {
+    generalStatBar.innerHTML = "Connection State: Connected";
     meetingNoInputField.disabled = false;
     joinMeetingBtn.disabled = false;
     leaveMeetingBtn.disabled = true;
@@ -442,7 +444,7 @@ function appStatusReceived(json) {
 }
 
 
-/* HOLDING SLIDE BUTTONS */
+/* STREAM BUTTONS */
 let pendingBtn = document.getElementById("pending-btn");
 pendingBtn.addEventListener("click", onPendingClick);
 let liveBtn = document.getElementById("live-btn");
@@ -452,7 +454,7 @@ technicalDiffBtn.addEventListener("click", onTechnicalDiff);
 let archiveBtn = document.getElementById("archive-btn")
 archiveBtn.addEventListener("click", onArchiveClick);
 
-/* HOLDING SLIDE BUTTON IMPLEMENTATION */
+/* STREAM BUTTON IMPLEMENTATION */
 function onPendingClick() {
   sendClickEvent(myVideoPlayer, OperatorControls._PendingButton)
 }
@@ -484,6 +486,7 @@ function onJoinClick() {
 }
 
 function onLeaveClicked() {
+  
   sendClickEvent(myVideoPlayer, OperatorControls._LeaveMeetingButton);
 }
 
@@ -802,9 +805,73 @@ function validateSchema() {
   return true;
 }
 
+/* SLIDE CONTROLS */
+let slideFieldset = document.getElementById("slide-fieldset");
+let slideBtnContainer = document.getElementById("slide-btn-container");
+let slideSwitchBtn = document.getElementById("slide-btn-element");
+let slideSwitchBtns = [];
+let slideClearBtn = document.getElementById("slide-clear-btn");
+slideClearBtn.addEventListener("click", onSlideClearClicked);
+
+let intro_preview = document.getElementById("intro-preview");
+let techdiff_preview = document.getElementById("techdiff-preview");
+let conc_preview = document.getElementById("conc-preview");
 
 
-/* DELETE BUTTON */
+/** Scratch work on logic */
+if (localStorage["currentIntroSlide"] !== "") {
+  intro_preview.style.backgroundImage = `url("/slides/${localStorage["currentIntroSlide"]}")`;
+} 
+if (localStorage["currentTechDiffSlide"] !== "") {
+  techdiff_preview.style.backgroundImage = `url("/slides/${localStorage["currentTechDiffSlide"]}")`;
+}
+if (localStorage["currentConclusionSlide"] !== "") {
+  conc_preview.style.backgroundImage = `url("/slides/${localStorage["currentConclusionSlide"]}")`;
+}
+
+function onSlideClearClicked() {
+  sendClickEvent(myVideoPlayer, OperatorControls._LiveButton);
+}
+
+slideSwitchBtn.style.display = "none";
+
+function setupSlideSetAsOptionsButton(owner) {
+  let slideSetAsIntro = document.querySelector(`div#${owner.id} a[target="action-set-as-intro"]`);
+  let slideSetAsTechDiff = document.querySelector(`div#${owner.id} a[target="action-set-as-techdiff"]`);
+  let slideSetAsConclusion= document.querySelector(`div#${owner.id} a[target="action-set-as-conclusion"]`);
+
+  slideSetAsIntro.addEventListener("click", (e)=>{
+    intro_preview.style.backgroundImage = `url("/slides/${owner.childNodes[1].innerHTML}")`;
+    localStorage.setItem("currentIntroSlide", owner.childNodes[1].innerHTML);
+  });
+  
+  slideSetAsTechDiff.addEventListener("click", (e)=>{
+    techdiff_preview.style.backgroundImage = `url("/slides/${owner.childNodes[1].innerHTML}")`;
+    localStorage.setItem("currentTechDiffSlide", owner.childNodes[1].innerHTML);
+  });
+  
+  slideSetAsConclusion.addEventListener("click", (e)=>{
+    conc_preview.style.backgroundImage = `url("/slides/${owner.childNodes[1].innerHTML}")`;
+    localStorage.setItem("currentConclusionSlide", owner.childNodes[1].innerHTML);
+  });
+}
+
+function checkToClearPreviewOnSlideDelete(owner) {
+  if (localStorage["currentIntroSlide"] === owner.childNodes[1].innerHTML) {
+    intro_preview.style.backgroundImage = "";
+    localStorage["currentIntroSlide"] = "";
+  } 
+  if (localStorage["currentTechDiffSlide"] === owner.childNodes[1].innerHTML) {
+    techdiff_preview.style.backgroundImage = ""
+    localStorage["currentTechDiffSlide"] = "";
+  } 
+  if (localStorage["currentConclusionSlide"] === owner.childNodes[1].innerHTML) {
+    conc_preview.style.backgroundImage = ""
+    localStorage["currentConclusionSlide"] = "";
+  }
+}
+
+// slide delete pill button.
 function setupDeleteButton(owner, route, spanWithFilename) {
   let deleteBtn = document.querySelector(`#${owner.id} .media-delete-btn`);
   let ogDeleteContents = deleteBtn.innerHTML;
@@ -831,27 +898,10 @@ function setupDeleteButton(owner, route, spanWithFilename) {
         //Reset delete button.
         deleteBtn.innerHTML = ogDeleteContents;
       });
+      checkToClearPreviewOnSlideDelete(owner);
     }
   });
 }
-
-/* SLIDE CONTROLS */
-let slideFieldset = document.getElementById("slide-fieldset");
-let slideBtnContainer = document.getElementById("slide-btn-container");
-let slideSwitchBtn = document.getElementById("slide-btn-element");
-let slideSwitchBtns = [];
-let slideClearBtn = document.getElementById("slide-clear-btn");
-slideClearBtn.addEventListener("click", onSlideClearClicked);
-
-let intro_preview = document.getElementById("intro-preview");
-let techdiff_preview = document.getElementById("techdiff-preview");
-let conc_preview = document.getElementById("conc-preview");
-
-function onSlideClearClicked() {
-  sendClickEvent(myVideoPlayer, OperatorControls._LiveButton);
-}
-
-slideSwitchBtn.style.display = "none";
 
 function validateSlideSwitchBtns(slides) {
   // Be sure there are enough buttons for slides.
@@ -865,6 +915,7 @@ function validateSlideSwitchBtns(slides) {
       slideSwitchBtns.push(clone);
       let span = document.querySelector(`#${clone.id} span`)
       //let deleteBtn = document.querySelector(`#${clone.id} .media-delete-btn`); // todo: make a function to create delete buttons.
+      setupSlideSetAsOptionsButton(clone);
       setupDeleteButton(clone, "/slide_delete", span);
       let button1 = document.querySelector(`#${clone.id} .media-left-btn`);
       let button2 = document.querySelector(`#${clone.id} .media-right-btn`);
@@ -1010,9 +1061,7 @@ batchSlideFileInput.addEventListener("change", batchFileInputChanged) // todo ma
 let batchSlideUploadBtn = document.getElementById("batch-slide-upload-btn") // todo Make this function less specific as well. Verify files based on extension.
 batchSlideUploadBtn.addEventListener("click", uploadCustomSlideClicked)
 
-/** SLIDE BROWSE CONTROLS */
-
-/** HOLD MUSIC BROWSE CONTROLS */
+/** HOLD MUSIC PREVIEW CONTROLS */
 let holdMusicSelect = document.getElementById("hold-music-select")
 let holdMusicOptionGroup = document.getElementById("hold-music-options-group")
 holdMusicSelect.addEventListener("change", UpdateHoldMusicBrowsePreviewElement)
@@ -1045,10 +1094,11 @@ function UpdateBrowsePreviewElement(lmtRoute, element, select, srcRoute) {
 }
 
 function UpdateSlideBrowsePreviewElement() {
-  UpdateEachSlidePreview("/last_slide_update", intro_preview, "intro")
-  UpdateEachSlidePreview("/last_slide_update", techdiff_preview, "technicalDifficulty")
-  UpdateEachSlidePreview("/last_slide_update", conc_preview, "conclusion")
+  // UpdateEachSlidePreview("/last_slide_update", intro_preview, "intro")
+  // UpdateEachSlidePreview("/last_slide_update", techdiff_preview, "technicalDifficulty")
+  // UpdateEachSlidePreview("/last_slide_update", conc_preview, "conclusion")
 }
+
 function UpdateHoldMusicBrowsePreviewElement() {
   UpdateBrowsePreviewElement("/last_holding_music_update", holdMusicAudioPlayer, holdMusicSelect, "/music")
 }
@@ -1305,7 +1355,7 @@ function uploadCustomSlideClicked() {
       formData.append(input.name, input.file)
   
       let request = new XMLHttpRequest();
-      createUploadProgressTracker(parentTracker, request, input.name, FetchAllUploadedMediaAndUpdateDash);
+      createUploadProgressTracker(parentTracker, request, input.name);
       request.onload = function() {
         if (request.status >= 200 && request.status < 300) {
           resolve(request.response);
@@ -1322,6 +1372,7 @@ function uploadCustomSlideClicked() {
   // After all files are done uploading re-enable upload button.
   Promise.all(uploads).then(() => {
     batchSlideUploadBtn.disabled = false;
+    FetchAllUploadedMediaAndUpdateDash();
   })
 
 }
