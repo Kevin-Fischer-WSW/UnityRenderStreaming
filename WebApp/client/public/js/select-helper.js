f357d8cfa9a1dcbdae8d3c09d29bfaf1c3aefd13
Kevin Fischer <106684807+Kevin-Fischer-WSW@users.noreply.github.com>
2023-09-26 10:45:28 -0700

    Added express backend to get and set a comma separated list of stream keys.

---
+++ WebApp/client/public/js/select-helper.js
@@ -0,0 +1,10 @@
+﻿export function selectHasOption(select, option) {
+  return Array.from(select.options).some(o => o.value === option);
+}
+
+export function createOption(optionValue, optionInnerHTML) {
+  const opt = document.createElement('option');
+  opt.value = optionValue;
+  opt.innerHTML = optionInnerHTML;
+  return opt;
+}
---
+++ WebApp/src/streamkey.ts
@@ -0,0 +1,9 @@
+﻿let streamkeys : string = "";
+
+export function setStreamkeys(keys: string) {
+    streamkeys = keys;
+}
+
+export function getStreamkeys() : string[] {
+    return streamkeys.split(';');
+}
